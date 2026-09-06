658beaddbe777a3b846a6e755d39bccfe011ab14
Gowthami516 <bulipegowthami@gmail.com>
2026-09-06 14:49:13 +0530

    Oops programs

---
+++ src/oops/CloneShallowCopy.java
@@ -0,0 +1,56 @@
+package oops;
+
+ class student implements Cloneable {
+	
+	
+	int sid;
+	String sname;
+	Address address;
+	
+	
+	@Override
+	protected Object clone() throws CloneNotSupportedException {
+		
+		return super.clone();
+	}
+	
+	public student(int sid,String sname,Address address) {
+		this.sid = sid;
+		this.sname = sname;
+		this.address = address;
+		}
+
+}
+ 
+ class Address {
+	 String city;
+  public Address(String city) {
+	this.city = city;
+}
+}
+	 public class CloneShallowCopy{
+		 public static void main(String[] args) throws CloneNotSupportedException {
+		Address address = new Address("Guntur");
+		student s1 = new student(16,"anu",address);
+		System.out.println(s1.sid);
+		System.out.println(s1.sname);
+		System.out.println(s1.address.city);
+		
+		student s2 = (student) s1.clone();
+		System.out.println(s2.sid);
+		System.out.println(s2.sname);
+		System.out.println(s2.address.city);
+		System.out.println("************************************");
+		
+		s2.sname = "Gowthami";
+		s2.address.city = "hyd";
+		
+		System.out.println(s1.sid);
+		System.out.println(s1.sname);
+		System.out.println(s1.address.city);
+		
+		System.out.println(s2.sid);
+		System.out.println(s2.sname);
+		System.out.println(s2.address.city);	
+	}
+}
